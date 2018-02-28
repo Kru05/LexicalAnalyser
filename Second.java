24f5d5c3068ce6ef130cc9a67c83f0820c59a08a
Krutee Dhekane <kruteedhekane5@gmail.com>
2018-02-28 14:09:01 +0530

    Initial commit

---
+++ Second.java
@@ -0,0 +1,17 @@
+public class SecondJava
+{
+//This is a comment
+public static void main(String args[])
+{
+int a = 5, b = 6 , c = 0;
+c = a + b;
+System.out.println("c = "+ c);
+}
+/*
+whatever comment
+*/
+}
+class First
+{
+System.out.println("Hii How are you");
+}
